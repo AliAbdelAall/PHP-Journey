2c215a2901a259e0dde8fc28365f62d10650d42a
AliAbdelAall <3laych@gmail.com>
2024-03-11 22:07:00 +0200

    added evaluateOperation function

--- eval-operation.php
+++ eval-operation.php
@@ -19,7 +19,59 @@ function validateParatheses($operation) {
 }
 
 
+function evaluateOperation($operation){
+  $valid = validateParatheses($operation);
+  $length = strlen($operation);
+  $num = 0;
+  $result = 0;
+  $operator = '';
+  $first_op = true;
+  $previous_num = false;
+  if($valid){
+    for($i = 0; $i < $length; $i++){
+      $char = $operation[$i];
+      if($char != "" && $char != "(" && $char != ")"){
+        if(ctype_digit($char)){
 
+          if($previous_num){
+            $num = $num * 10 + intval($char);
+          }else{
+            $num = intval($char);
+            $previous_num = true;;
+          }
+        }else {
+          $operator = $operation[$i];
+          echo $operator;
+        }
+        if(!$operator && $first_op){
+          $result += $num;
+        }else{
+          if (($operator !== '' && $num != 0) || $i === $length - 1)  {
+            switch($operator) {
+              case "+":
+                $result += $num;
+                break;
+              case "-":
+                $result -= $num;
+                break;
+              case "*":
+                $result *= $num;
+                break;
+              case "/":
+                $result /= $num;
+                break;
+              } 
+            }
+          }
+        $num = 0;
+        $previous_num = true;
+      } 
+    }
+  }else{
+    echo "INVALID Operation";
+  }
+  return $result;
+}
 
-$operation = "()()()()";
-echo validateParatheses($operation);
+$operation = "(5+4-3*3/2)";
+echo evaluateOperation($operation);
